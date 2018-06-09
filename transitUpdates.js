d5c2eaa294429e74ebb655949a252d5e7e5ff1c4
jfabian <jfabian@mbta.com>
2018-06-09 14:12:22 -0400

    Display route name using official colors

--- transitUpdates.js
+++ transitUpdates.js
@@ -15,23 +15,37 @@ var serviceUpdate = function nextServiceUpdate() {
 
     jQuery(document).ready(function($) {
         $.ajax({
-            url : "https://api-v3.mbta.com/predictions?include=trip&sort=departure_time" + stopsFilter + routesFilter,
+            url : "https://api-v3.mbta.com/predictions?include=trip,route&sort=departure_time" + stopsFilter + routesFilter,
             dataType : "json",
             success : function(parsed_json) {
 
                 var allPredictions = parsed_json['data'];
-                var allTrips = parsed_json['included'];
+                var allIncluded = parsed_json['included'];
                 var predictions = [];
                 var infoAboutTrips = {};
+                var infoAboutRoutes = {};
                 var htmlForPredictions = '';
 
-                for (i = 0; i < allTrips.length; i++) {
-                    var tripId = allTrips[i]['id'];
-                    var tripHeadsign = allTrips[i]['attributes']['headsign'];
-                    var tripName = allTrips[i]['attributes']['name'];
-                    infoAboutTrips[tripId] = {};
-                    infoAboutTrips[tripId]['headsign'] = tripHeadsign;
-                    infoAboutTrips[tripId]['name'] = tripName;
+                for (i = 0; i < allIncluded.length; i++) {
+                    if (allIncluded[i]['type'] == 'trip') {
+                        var tripId = allIncluded[i]['id'];
+                        var tripHeadsign = allIncluded[i]['attributes']['headsign'];
+                        var tripName = allIncluded[i]['attributes']['name'];
+                        infoAboutTrips[tripId] = {};
+                        infoAboutTrips[tripId]['headsign'] = tripHeadsign;
+                        infoAboutTrips[tripId]['name'] = tripName;
+                    } else if (allIncluded[i]['type'] == 'route') {
+                        var routeId = allIncluded[i]['id'];
+                        var textColor = allIncluded[i]['attributes']['text_color'];
+                        var backgroundColor = allIncluded[i]['attributes']['color'];
+                        var longName = allIncluded[i]['attributes']['long_name'];
+                        var shortName = allIncluded[i]['attributes']['short_name'];
+                        infoAboutRoutes[routeId] = {};
+                        infoAboutRoutes[routeId]['textColor'] = textColor;
+                        infoAboutRoutes[routeId]['backgroundColor'] = backgroundColor;
+                        infoAboutRoutes[routeId]['longName'] = longName;
+                        infoAboutRoutes[routeId]['shortName'] = shortName;
+                    }
                 }
 
                 for (i = 0; i < allPredictions.length; i++) {
@@ -40,6 +54,12 @@ var serviceUpdate = function nextServiceUpdate() {
                     }
                     var stopId = allPredictions[i]['relationships']['stop']['data']['id'];
                     var routeId = allPredictions[i]['relationships']['route']['data']['id'];
+                    var routeDisplay = infoAboutRoutes[routeId]['shortName'];
+                    if (routeDisplay == '') {
+                        routeDisplay = infoAboutRoutes[routeId]['longName'];
+                    }
+                    var textColor = infoAboutRoutes[routeId]['textColor'];
+                    var backgroundColor = infoAboutRoutes[routeId]['backgroundColor'];
                     var tripId = allPredictions[i]['relationships']['trip']['data']['id'];
                     var headsign = infoAboutTrips[tripId]['headsign'];
                     if (headsign == 'University Park') {
@@ -58,8 +78,12 @@ var serviceUpdate = function nextServiceUpdate() {
                     } else {
                         countdown = Math.round(rawCountdown/60) + 'm';
                     }
+                    console.log(textColor)
+                    console.log(backgroundColor)
                     htmlForPredictions += '<span class="prediction">'
-                    htmlForPredictions += '<span class="route">' + routeId + '</span>&nbsp;';
+                    htmlForPredictions += '<span class="route" style="color: #' + textColor;
+                    htmlForPredictions += '; background-color: #' + backgroundColor + '">&nbsp;';
+                    htmlForPredictions += routeDisplay + '&nbsp;</span>&nbsp;';
                     htmlForPredictions += '<span class="headsign">' + headsign + '</span>&nbsp';
                     htmlForPredictions += '<span class="countdown">' + countdown + '</span>&nbsp';
                     htmlForPredictions += '</span><br/>';
