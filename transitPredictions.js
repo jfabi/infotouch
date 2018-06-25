b6da1f80575b8aba2e3fcc764b4908d17cbc989d
jfabian <jfabian@mbta.com>
2018-06-25 01:11:17 -0400

    Show only up to two predictions per route-headsign combo

--- transitPredictions.js
+++ transitPredictions.js
@@ -26,6 +26,7 @@ var transitPredictionsUpdate = function nextServiceUpdate() {
                 var infoAboutRoutes = {};
                 var htmlForPredictions = '';
                 var htmlForAlerts = '';
+                var displayablePredictions = {};
 
                 for (i = 0; i < allIncluded.length; i++) {
                     if (allIncluded[i]['type'] == 'trip') {
@@ -55,12 +56,6 @@ var transitPredictionsUpdate = function nextServiceUpdate() {
                     }
                     var stopId = allPredictions[i]['relationships']['stop']['data']['id'];
                     var routeId = allPredictions[i]['relationships']['route']['data']['id'];
-                    var routeDisplay = infoAboutRoutes[routeId]['shortName'];
-                    if (routeDisplay == '') {
-                        routeDisplay = infoAboutRoutes[routeId]['longName'];
-                    }
-                    var textColor = infoAboutRoutes[routeId]['textColor'];
-                    var backgroundColor = infoAboutRoutes[routeId]['backgroundColor'];
                     var tripId = allPredictions[i]['relationships']['trip']['data']['id'];
                     var headsign = infoAboutTrips[tripId]['headsign'];
                     if (headsign == 'University Park') {
@@ -79,12 +74,49 @@ var transitPredictionsUpdate = function nextServiceUpdate() {
                     } else {
                         countdown = Math.round(rawCountdown/60) + 'm';
                     }
+
+                    routeHeadsignKey = routeId + '-' + headsign;
+                    // Show at most 2 upcoming predictions per route-headsign combination
+                    if (!displayablePredictions[routeHeadsignKey]) {
+                        displayablePredictions[routeHeadsignKey] = {};
+                        displayablePredictions[routeHeadsignKey]['routeId'] = routeId;
+                        displayablePredictions[routeHeadsignKey]['headsign'] = headsign;
+                        displayablePredictions[routeHeadsignKey]['predictions'] = []
+                        displayablePredictions[routeHeadsignKey]['predictions'].push(countdown);
+                    } else if (displayablePredictions[routeHeadsignKey]['predictions'].length < 2) {
+                        displayablePredictions[routeHeadsignKey]['routeId'] = routeId;
+                        displayablePredictions[routeHeadsignKey]['headsign'] = headsign;
+                        displayablePredictions[routeHeadsignKey]['predictions'].push(countdown);
+                    }
+                }
+
+                for (var key in displayablePredictions) {
+                    if (!displayablePredictions.hasOwnProperty(key)) {
+                        //The current property is not a direct property of p
+                        continue;
+                    }
+                    var routeId = displayablePredictions[key]['routeId'];
+                    var routeDisplay = infoAboutRoutes[routeId]['shortName'];
+                    if (routeDisplay == '') {
+                        routeDisplay = infoAboutRoutes[routeId]['longName'];
+                    }
+                    var textColor = infoAboutRoutes[routeId]['textColor'];
+                    var backgroundColor = infoAboutRoutes[routeId]['backgroundColor'];
+
                     htmlForPredictions += '<span class="prediction">'
                     htmlForPredictions += '<span class="route" style="color: #' + textColor;
                     htmlForPredictions += '; background-color: #' + backgroundColor + '">&nbsp;';
                     htmlForPredictions += routeDisplay + '&nbsp;</span>&nbsp;';
-                    htmlForPredictions += '<span class="headsign">' + headsign + '</span>&nbsp';
-                    htmlForPredictions += '<span class="countdown">' + countdown + '</span>&nbsp';
+                    htmlForPredictions += '<span class="headsign">' + displayablePredictions[key]['headsign'] + '</span>&nbsp';
+                    htmlForPredictions += '<span class="countdown">';
+                    for (i = 0; i < displayablePredictions[key]['predictions'].length; i++) {
+                        htmlForPredictions += displayablePredictions[key]['predictions'][i];
+                        if (i + 1 < displayablePredictions[key]['predictions'].length) {
+                            // Separate out each predictions
+                            htmlForPredictions += ', ';
+                        }
+                    }
+                    htmlForPredictions += '</span>&nbsp';
                     htmlForPredictions += '</span><br/>';
                 }
 
